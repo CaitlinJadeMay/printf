e98aefe21acffae1731e5b3a2c65ba5417d0073b
CaitlinJadeMay <caitlinjadelingeveldt@gmail.com>
2023-05-30 14:44:57 -0700

    Added changes to habndle files anf _printf

---
+++ test/handle_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * handle_str - handles string format
+ * @s: variable list for %s format
+ * Return: return 1 if true and 0 if not
+ */
+
+int handle_str(va_list s)
+{
+	char *str = va_arg(s, char *);
+	int i = 0;
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
---
+++ test/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+  * struct convert - typedef for printing various type
+  * @a: type of print
+  * @b: function to print
+  */
+typedef struct convert
+{
+	char *a;
+	int (*b)(va_list);
+} convert;
+
+/** Functions prototypes for main operations **/
+int _printf(const char *format, ...);
+int _putchar(char c);
+int handle_char(va_list c);
+int handle_str(va_list s);
+/** End of function prototypes **/
+
+#endif
