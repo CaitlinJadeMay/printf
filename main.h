655aed00cd6c5811930093b4ee8814adf9a56314
Isabel <18isabelbanda@gmail.com>
2023-06-01 16:07:31 -0700

    editing main.h

--- main.h
+++ main.h
@@ -24,21 +24,6 @@ int handle_dec(va_list d);
 int handle_binary(va_list b);
 int handle_float(va_list f);
 int handle_e(va_list e);
-int handle_g(va_list g);
-int handle_l(va_list l);
-int handle_zero(va_list zero);
-int handle_hex(va_list h);
-int handle_u(va_list u);
-int handle_octa(va_list o);
-int handle_x(va_list x);
-int handle_X(va_list X);
-int handle_p(va_list p);
-int handle_S(va_list S);
 int handle_rev(va_list r);
-int handle_R(va_list R);
-int handle_add(va_list add);
-int handle_less(va_list less);
-int handle_space(va_list space);
-int handle_sharp(va_list sharp);
 
 #endif
